7fcf80c3e3f65421ba88f65d601b7a1874c0d43d
jsegarradev <87569388+jsegarradev@users.noreply.github.com>
2023-12-08 03:03:38 +0100

    Update 0xelkot.sh

--- 0xelkot.sh
+++ 0xelkot.sh
@@ -1,23 +1,22 @@
 #!/bin/bash 
 
 domain=$1
-mkdir /root/$1
-mkdir /root/$1/xray
+mkdir ~/BB/$1
+mkdir ~/BB/$1/xray
 
-subfinder -d $1 -silent | anew /root/$1/subs.txt
-assetfinder -subs-only $1 | anew /root/$1/subs.txt
-amass enum -passive -d $1 | anew /root/$1/subs.txt
+subfinder -d $1 -silent | anew ~/BB/$1/subs.txt
+assetfinder -subs-only $1 | anew ~/BB/$1/subs.txt
+
                                                                                                                                                                                                              
-                                                                                                                                                                                                             
-                                                                                                                                                                                                             
-cat /root/$1/subs.txt | httpx -silent | anew /root/$1/alive.txt              
-                                                                                                                                                                                                                                                                                                                                                                                                                      
+cat ~/BB/$1/subs.txt | httpx -silent | anew ~/BB/$1/alive.txt              
+                                                                                                                                                                                                                                        >
 
 ## Test by Xray 
 
-for i in $(cat /root/$1/alive.txt); do xray_linux_amd64 ws --basic-crawler $i --plugins xss,sqldet,xxe,ssrf,cmd-injection,path-traversal --ho $(date +"%T").html ; done 
+## for i in $(cat /home/kali/BB/$1/alive.txt); do xray_linux_amd64 ws --basic-crawler $i --plugins xss,sqldet,xxe,ssrf,cmd-injection,path-traversal --ho $(date +"%T").html ; done 
   
 
 ## test for nuclei 
 
-cat /root/$1/alive.txt | nuclei -t /root/cent-nuclei-templates -es info,unknown -etags ssl,network | anew /root/$1/nuclei.txt
+cat ~/BB/$1/alive.txt | nuclei -t ~/cent-nuclei-templates -es info,unknown -etags ssl,network | anew ~/BB/$1/nuclei.txt
+
