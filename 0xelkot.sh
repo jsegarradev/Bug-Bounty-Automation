37c743957b4623b7ad0a8e9374ddbc0f6fb97269
jsegarradev <87569388+jsegarradev@users.noreply.github.com>
2023-12-08 16:30:23 +0100

    optional xray

--- 0xelkot.sh
+++ 0xelkot.sh
@@ -1,18 +1,24 @@
-#!/bin/bash 
+#!/bin/bash
 
 domain=$1
-mkdir ~/BB/$1
-mkdir ~/BB/$1/xray
+use_xray=$2
 
-subfinder -d $1 -silent | anew ~/BB/$1/subs.txt
-assetfinder -subs-only $1 | anew ~/BB/$1/subs.txt
+mkdir ~/BB/$domain
+mkdir ~/BB/$domain/xray
 
-cat ~/BB/$1/subs.txt | httpx -silent | anew ~/BB/$1/alive.txt
+# Collect subdomains
+subfinder -d $domain -silent | anew ~/BB/$domain/subs.txt
+assetfinder -subs-only $domain | anew ~/BB/$domain/subs.txt
 
-## Test by Xray
+# Check for alive subdomains
+cat ~/BB/$domain/subs.txt | httpx -silent | anew ~/BB/$domain/alive.txt
 
-## for i in $(cat /home/kali/BB/$1/alive.txt); do xray_linux_amd64 ws --basic-crawler $i --plugins xss,sqldet,xxe,ssrf,cmd-injection,path-traversal --ho $(date +"%T").html ; done
+# Test by Xray if specified
+if [ "$use_xray" == "--xray" ]; then
+    for i in $(cat ~/BB/$domain/alive.txt); do
+        xray_linux_amd64 ws --basic-crawler $i --plugins xss,sqldet,xxe,ssrf,cmd-injection,path-traversal --ho $(date +"%T").html
+    done
+fi
 
-## test for nuclei 
-
-cat ~/BB/$1/alive.txt | nuclei -t ~/cent-nuclei-templates -es info,unknown -etags ssl,network | anew ~/BB/$1/nuclei.txt
+# Test for nuclei
+cat ~/BB/$domain/alive.txt | nuclei -t ~/cent-nuclei-templates -es info,unknown -etags ssl,network | anew ~/BB/$domain/nuclei.txt
